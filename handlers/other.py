3116a6f392676213b1e4357af36343e21b56a1b4
Zavolzhenskyi Ivan <ven.volga@gmail.com>
2023-06-02 09:50:44 +0300

    Echo function and TODO list

--- handlers/other.py
+++ handlers/other.py
@@ -1,8 +1,7 @@
 from aiogram import types, Dispatcher
-from create_bot import dp, bot
 
 
-# @dp.message_handler()
+# TODO write welcome message to unknown command
 async def echo_send(message: types.Message):
     if message.text == "Hello!":
         await message.answer("Hello my little rider!")
@@ -10,6 +9,5 @@ async def echo_send(message: types.Message):
         await message.answer(message.text)
 
 
-def register_handler_other(dp: Dispatcher):  # реєстрація хендлерів у файлі
+def register_handler_other(dp: Dispatcher):
     dp.register_message_handler(echo_send)
-    #next handler
--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import executor
 from create_bot import dp
 from data_and_metrics.client_requests import schedule_log_task, upload_requests_log
-from handlers import client, other, admin
+from handlers import client, admin, other
 from datetime import datetime
 from informations.resorts_data import get_db_data
 
@@ -10,20 +10,19 @@ from informations.resorts_data import get_db_data
 async def on_startup(_: dp):
     print("Bot is online! Started at", datetime.now().strftime('%d %B %G %H:%M:%S'))
     await get_db_data()
-    schedule_task = asyncio.create_task(schedule_log_task())  # Запуск відкладеної функції у асинхронній петлі
-    await schedule_task  # Очікування завершення відкладеної задачі
+    schedule_task = asyncio.create_task(schedule_log_task())
+    await schedule_task
 
 
 async def on_shutdown(_: dp):
     await upload_requests_log()
 
 
-# імпорт функцій з модулів client, admin, other
 client.register_handler_client(dp)
 admin.register_handler_admin(dp)
-# other.register_handler_other(dp)
+other.register_handler_other(dp)
 
-# Запуск бота з використанням асинхронної петлі asyncio
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     try:
