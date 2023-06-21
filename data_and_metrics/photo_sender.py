c83b11c81e5e8f5f4d5691a1d6fd66b8f46e293a
Zavolzhenskyi Ivan <ven.volga@gmail.com>
2023-06-21 14:34:09 +0300

    New path to welcome photos

--- data_and_metrics/photo_sender.py
+++ data_and_metrics/photo_sender.py
@@ -1,13 +1,13 @@
 welcome_photo_path = {
-            'Славське': 'data_and_metrics/welcome_photos/slavske.jpeg',
-            'Драгобрат': 'data_and_metrics/welcome_photos/dragobrat.jpeg',
-            'Буковель': 'data_and_metrics/welcome_photos/bukovel.jpeg',
-            'Пилипець': 'data_and_metrics/welcome_photos/pylypec.jpeg',
-            'Плай': 'data_and_metrics/welcome_photos/play.jpeg',
-            'Яблуниця': 'data_and_metrics/welcome_photos/yablunitca.jpeg',
-            'Красія': 'data_and_metrics/welcome_photos/krasiya.jpeg',
-            'Мигове': 'data_and_metrics/welcome_photos/migove.jpeg',
-            'Яремче': 'data_and_metrics/welcome_photos/yaremche.jpeg',
+            'Славське': './data_and_metrics/welcome_photos/slavske.jpeg',
+            'Драгобрат': './data_and_metrics/welcome_photos/dragobrat.jpeg',
+            'Буковель': './data_and_metrics/welcome_photos/bukovel.jpeg',
+            'Пилипець': './data_and_metrics/welcome_photos/pylypec.jpeg',
+            'Плай': './data_and_metrics/welcome_photos/play.jpeg',
+            'Яблуниця': './data_and_metrics/welcome_photos/yablunitca.jpeg',
+            'Красія': './data_and_metrics/welcome_photos/krasiya.jpeg',
+            'Мигове': './data_and_metrics/welcome_photos/migove.jpeg',
+            'Яремче': './data_and_metrics/welcome_photos/yaremche.jpeg',
 }
 
 
